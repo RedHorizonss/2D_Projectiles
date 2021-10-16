9a4382fb730c1aee188fc40a6d4898b18ea06e3b
Fay <fay.a.timen@gmail.com>
2021-10-16 12:17:14 +0100

    Added update state to program, updates x and y each time step

--- projectile.py
+++ projectile.py
@@ -1,2 +1,14 @@
 #Main projectile python package
 
+def update_state(t, x, v, a, dt=0.1):
+    #Update each parameter for the next time step which is dt
+        
+    distance_moved = v*dt + (1/2)*a*(dt**2) # Calculates distance moved through SUVAT
+    x += distance_moved
+    #Calulates new velocity and time due to the movement
+    v += a*dt
+    t += dt
+    
+    return t, x, v
+     
+    
