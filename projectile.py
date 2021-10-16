f0f76ff614f9981659f5dc646fadd5c58230cc34
Fay <fay.a.timen@gmail.com>
2021-10-16 12:19:02 +0100

    Added calculate accel y/x, calculates the force acting on the objects

--- projectile.py
+++ projectile.py
@@ -10,5 +10,20 @@ def update_state(t, x, v, a, dt=0.1):
     t += dt
     
     return t, x, v
-     
+
+from numpy import sign
+
+def calculate_acceleration_y(v, k=0.0, mass, gravity=-9.81):
+    #Calculates acceleration at each time step (based on the velocity at that time)
+    
+    force_gravity = mass*gravity #F = ma calculates the force of gravity acting on y currently
+    force_air = -sign(v)*k*v**2 #F=-kv^2
+    total_force = force_gravity + force_air
+    a = total_force/mass #a = F/m calculates acceleration with total force (gravity and air resistance)
+    
+    return a
+
+def calculate_acceleration_x(v, k=0.0, mass):
+    force = -sign(v)*k*v**2 #F=-kv^2
+    a = force/mass #a = F/m calculates acceleration with force
     
