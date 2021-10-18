a78afaaf84926426936b92c81882651ed47d9364
Fay <fay.a.timen@gmail.com>
2021-10-18 16:33:54 +0100

    Commented and correctly programmed plots, fixed syntax issues

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,24 +1,26 @@
 #Package to plot python
 import matplotlib.pyplot as plt
 
-def plot_xy(x, y, vx, vy):
+def plot_xy(x, y, vx, vy, t):
 
-    fig, axs = plt.subplots(1, 2)
+    fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize = (10,10))
     
-    #Plots 
-    ax1.plot(x, y, title = "Trajectory of projectile")
+    #Plots The trajectory 
+    ax1.plot(x, y)
+    ax1.set_title("Trajectory of projectile")
     ax1.set_ylabel("y position (m)")
     ax1.set_xlabel("x position (m)")
-    plt.show()
     
-    ax2.plot(t, vx, title = "Change in Velocity for x")
+    #Plots The change in velocity for x and y
+    ax2.plot(t, vx)
+    ax2.set_title("Change in Velocity for x")
     ax2.set_ylabel("Velocity of x (m/s)")
     ax2.set_xlabel("Time (s)")
-    plt.show()
     
-    ax3.plot(t, vy, title = "Chnage in Velocity for y")
+    ax3.plot(t, vy)
+    ax3.set_title("Change in Velocity for y")
     ax3.set_ylabel("Velocity of y (m/s)")
     ax3.set_xlabel("Time (s)")
     plt.show()
-    
+
     
