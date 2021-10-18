c0230b78498c1f8eead8eaf3a6bfb5193ede1b08
Fay <fay.a.timen@gmail.com>
2021-10-18 16:18:14 +0100

    Changed the inputs to the function, added 1 more plot

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,7 +1,7 @@
 #Package to plot python
 import matplotlib.pyplot as plt
 
-def plot_xy(x, y):
+def plot_xy(x, y, vx, vy):
 
     fig, axs = plt.subplots(1, 2)
     
@@ -11,9 +11,14 @@ def plot_xy(x, y):
     ax1.set_xlabel("x position (m)")
     plt.show()
     
-    ax2.plot(vx,t)
+    ax2.plot(t, vx, title = "Change in Velocity for x")
     ax2.set_ylabel("Velocity of x (m/s)")
     ax2.set_xlabel("Time (s)")
+    plt.show()
+    
+    ax3.plot(t, vy, title = "Chnage in Velocity for y")
+    ax3.set_ylabel("Velocity of y (m/s)")
+    ax3.set_xlabel("Time (s)")
     plt.show()
     
     
